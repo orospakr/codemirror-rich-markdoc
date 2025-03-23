632e785615ba17e79e6a24d793d1367895ba3f01
Andrew Clunis <andrew@orospakr.ca>
2025-03-23 18:49:06 -0400

    remove --dts-resolve, I don't think it's necessary

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: {
-    resolve: true,
-  },
+  dts: true,
   sourcemap: true,
   splitting: false,
   clean: true,
